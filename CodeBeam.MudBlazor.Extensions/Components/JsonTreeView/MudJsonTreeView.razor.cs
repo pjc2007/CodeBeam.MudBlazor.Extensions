290d6dc93a788246235955ccab04ae6146fab82a
Jon Person <person.jon@gmail.com>
2024-07-19 04:22:49 -0600

    MudJsonTreeView: Uses a MudTreeView to View JSON Text (#378)
    * MudJsonTreeView: Uses a MudTreeView to view JSON
    
    * MudJsonTreeView: Added example, can't get page to not 404
    
    * MudJsonTreeView: Fixed example
    
    * Docs Implement
    
    ---------
    
    Co-authored-by: Jon Person <j.person@dodmarrow.org>
    Co-authored-by: Mehmet Can Karagöz <78308169+mckaragoz@users.noreply.github.com>

---
+++ CodeBeam.MudBlazor.Extensions/Components/JsonTreeView/MudJsonTreeView.razor.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System.Text.Json.Nodes;
+
+namespace MudExtensions;
+
+#nullable enable
+
+/// <summary>
+/// Represents a tree view which displays a snippet of JSON.
+/// </summary>
+public partial class MudJsonTreeView : MudComponentBase
+{
+    private string? _json;
+
+    /// <summary>
+    /// Gets or sets the JSON to be displayed.
+    /// </summary>
+    [Parameter]
+    [EditorRequired]
+    public string? Json 
+    {
+        get => _json;
+        set => SetJson(value);
+    }
+
+    /// <summary>
+    /// Sets the <see cref="Json"/> property and raises the <see cref="OnJsonChanged"/> event.
+    /// </summary>
+    /// <param name="json">The new JSON to use.</param>
+    protected void SetJson(string? json)
+    {
+        _json = json;
+        Root = string.IsNullOrEmpty(_json) ? null : JsonNode.Parse(_json);
+        OnJsonChanged.InvokeAsync(_json);
+        StateHasChanged();
+    }
+
+    /// <summary>
+    /// Occurs when the JSON has changed.
+    /// </summary>
+    public EventCallback<string> OnJsonChanged { get; set; }
+
+    /// <summary>
+    /// Gets or sets the root node of the JSON to display.
+    /// </summary>
+    public JsonNode? Root { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the tree contents are compacted.
+    /// </summary>
+    [Parameter]
+    public bool Dense { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the current row is highlighted.
+    /// </summary>
+    [Parameter]
+    public bool Hover { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether items are sorted by key.
+    /// </summary>
+    [Parameter]
+    public bool Sorted { get; set; }
+}
---
+++ CodeBeam.MudBlazor.Extensions/Components/JsonTreeView/MudJsonTreeViewNode.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Text.Json.Nodes;
+
+namespace MudExtensions;
+
+#nullable enable
+
+/// <summary>
+/// Represents the child leaf of a JSON tree view.
+/// </summary>
+public partial class MudJsonTreeViewNode : ComponentBase
+{
+    /// <summary>
+    /// Gets or sets the node to display (including children).
+    /// </summary>
+    [Parameter]
+    [EditorRequired]
+    public JsonNode? Node { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether items are sorted by key.
+    /// </summary>
+    [Parameter]
+    public bool Sorted { get; set; }
+}
