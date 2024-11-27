4850b7873565707020644fda87a08610273f78dc
Peter Chapman <pchapman@micromine.com>
2024-11-27 14:36:31 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AgGridAngular } from 'ag-grid-angular'; // Angular Data Grid Component
+import { ColDef } from 'ag-grid-community'; // Column Definition Type Interface
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AgGridAngular],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'oid-grid';
+
+  // Row Data: The data to be displayed.
+ rowData = [
+  { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+  { make: "Ford", model: "F-Series", price: 33850, electric: false },
+  { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+];
+
+// Column Definitions: Defines the columns to be displayed.
+colDefs: ColDef[] = [
+  { field: "make" },
+  { field: "model" },
+  { field: "price" },
+  { field: "electric" }
+];
 }
