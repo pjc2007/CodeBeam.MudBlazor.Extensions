85c6d58d971d8bbd65914855619f5acc12b234e0
Peter Chapman <pchapman@micromine.com>
2024-11-27 15:29:48 +0800

    Added valueGetter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,113 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { AgGridAngular } from 'ag-grid-angular'; // Angular Data Grid Component
-import { ColDef } from 'ag-grid-community'; // Column Definition Type Interface
+import { GridApi, GridOptions, GridReadyEvent } from 'ag-grid-community'; // Column Definition Type Interface
 
+type Data = { id: string, description: string, state: string; };
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AgGridAngular],
+  imports: [AgGridAngular],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'oid-grid';
+  title = 'my-grid';
 
-  // Row Data: The data to be displayed.
- rowData = [
-  { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-  { make: "Ford", model: "F-Series", price: 33850, electric: false },
-  { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-];
+  public gridOptions: GridOptions;
 
-// Column Definitions: Defines the columns to be displayed.
-colDefs: ColDef[] = [
-  { field: "make" },
-  { field: "model" },
-  { field: "price" },
-  { field: "electric" }
-];
+  public A: Data[];
+  public B: Data[];
+  public C: Data[];
+
+  private gridApi!: GridApi;
+  
+  constructor() {
+    this.A = [
+      { id: 'v1', description: 'v01', state: 'A' },
+      { id: 'v2', description: 'v02', state: 'A' }
+    ];
+
+    this.B = [
+      { id: 'v3', description: 'v03', state: 'B' },
+    ];
+
+    this.C = [
+      { id: 'v4', description: 'v04', state: 'C' },
+      { id: 'v5', description: 'v05', state: 'C' },
+      { id: 'v6', description: 'v06', state: 'C' }
+    ];
+
+    this.gridOptions = <GridOptions>{
+      enableSorting: true,
+      // enable filtering 
+      enableFilter: true
+    };
+    this.gridOptions.columnDefs = [
+      {
+        headerName: "A",
+        width: 100,
+        valueGetter: this.getData.bind(this)
+      },
+      {
+        headerName: "B",
+        width: 100,
+        valueGetter: this.getData.bind(this)
+      },
+      {
+        headerName: "C",
+        width: 100,
+        valueGetter: this.getData.bind(this)
+      },
+
+    ];
+    this.gridOptions.rowData = [
+      { id: 0 },
+      { id: 1 },
+      { id: 2 }
+    ];
+  }
+
+  public onGridReady(params: GridReadyEvent): void {
+    this.gridApi = params.api;
+  }
+
+  private getData(params: any): string | null {
+    const id = params.data.id;
+    const colName = params.colDef.headerName;
+    switch (colName) {
+      case 'A':
+        return this.getCellData(this.A, id);
+      case 'B':
+        return this.getCellData(this.B, id);
+      case 'C':
+        return this.getCellData(this.C, id);
+    }
+    return '';
+  }
+
+  private getCellData(list: Data[], row: number): string | null {
+    if (row >= list.length) {
+      return null;
+    }
+
+    return list[row].description;
+  }
+
+  public addToA(): void {
+    this.A.push({ id: 'v7', description: 'v07', state: 'A' });
+
+    const max = Math.max(this.A.length, this.B.length, this.C.length);
+    this.gridOptions.rowData?.push({ id: max - 1 });    
+    
+    var params = {
+      force: true,
+      suppressFlash: true,
+    };
+
+    //this.gridApi.updateGridOptions(this.gridOptions);
+    this.gridApi.refreshCells(params);
+    this.gridApi.refreshCells();
+    this.gridApi.redrawRows();
+  }
 }
+
